a1b92f4589ff5346b8f0566a6a4fe5dd28a625a5
Ryan Quinlan <apex2060@site4u2.com>
2016-11-01 21:41:21 +0000

    sample stripe fire integration

---
+++ cloud/main.js
@@ -0,0 +1,1 @@
+var stripe = require('cloud/stripe.js');
---
+++ cloud/stripe.js
@@ -0,0 +1,8 @@
+var stripeFire = require("stripe-fire")("sk_test_9I9XHDH7nBVcB3ola0vfOYCJ");
+
+var charges = stripeFire.charges("https://ldstreasury-6424b.firebaseio.com/stripe/charges", function(err, charge) {
+    // Called after a create/update charge request is sent to Stripe 
+}, "ACCESS_TOKEN", function(chargeData) {
+    // Called before a create/update charge request is sent to Stripe 
+    return chargeData;
+});
